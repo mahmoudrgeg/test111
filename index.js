7bbea32914b061a1b7c3da5520e40cbe78533645
mahmoudrgeg <80081827+mahmoudrgeg@users.noreply.github.com>
2021-03-05 20:11:05 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+
+app.listen(() => console.log("Starting..."));
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+///All Copy Right Reserved For: Shuruhatik  in YT
+
+const discord = require("discord.js");
+const shuruhatik = new discord.Client();
+const ytdl = require('ytdl-core');
+const moment = require("moment");
+const ms = require("ms");
+const pretty = require("pretty-ms");
+////////تعديلات  التذكير
+////////////بوت تذكير
+//////////لاتغير اشي فوق
+var azkarbyshuruhatik = {
+  imageembed: "",////لا تغير اشي
+  colorembed: "اسم الون",////لون رساله التذكير
+  channelname: "اسم الروم",//// اسم الروم الي بدك يكون الكلام فيها
+  time: "90000",////وقت الكلام
+};
+var facts =[
+ 'الكلام الي بدك اياه',
+] 
+
+
+//////لاتغيير اشي
+setInterval(() => {
+  let azkarchannel = shuruhatik.channels.cache.filter(c => c.name.toLowerCase() === azkarbyshuruhatik.channelname).random()
+  var fact = Math.floor(Math.random() * facts.length);
+ 	var embed = new discord.MessageEmbed()
+   .setColor(azkarbyshuruhatik.colorembed || "")
+  .setThumbnail(`${azkarbyshuruhatik.imageembed|| ""}`)
+  .setTitle("")///All Copy Right Reserved For: Shuruhatik  in YT
+  .setTimestamp()/////All Copy Right Reserved For: Shuruhatik  in YT
+  .setDescription(facts[fact])
+  .setFooter("")
+	azkarchannel.send(embed)
+}, azkarbyshuruhatik.time);
+
+///All Copy Right Reserved For: Shuruhatik  in YT
+
+shuruhatik.login(process.env.token);
